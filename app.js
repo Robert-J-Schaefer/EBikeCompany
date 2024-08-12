80db99034d5e334f950e6fa38276818609255011
Robert-J-Schaefer <rjschaefer90@gmail.com>
2024-08-12 04:13:10 -0400

    Finished Navbar

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const child = document.querySelector(".child");
 child.addEventListener('click', e => {
     
 });
+
+function showSideBar(){
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.style.display = 'flex';
+}
+
+function hideSideBar(){
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.style.display = 'none';
+}
