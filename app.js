c5af03a736f2d624eb8ffcb5cb4b2b3f051aa5d1
Robert-J-Schaefer <rjschaefer90@gmail.com>
2024-08-12 08:37:58 -0400

    finishing up

--- app.js
+++ app.js
@@ -18,36 +18,44 @@ canvasCtx.lineCap = 'round';
 let isDraw = false;
 // store the beginning of the line
 let lastX = 0;
-let lastY = o;
+let lastY = 0;
 
 //is run when user clicks inside of canvas
-canvas.addEventListener('mousedown', (event) =>{
+canvas.addEventListener('mousedown', (event) => {
+    init(event);
     isDraw = true;
-    const {offsetX, offsetY} = event;
-    lastX = offsetX;
-    lastY= offsetY;
+    console.log("True");
 })
-//is run when the user stops clicking inside of canvs 
-canvas.addEventListener('mousemove' , (event) => {
-    const{offsetX, offsetY} = event;
-    if(isDraw){
-        canvasCtx.beginPath();
-        canvasCtx.moveTo(lastX, lastY);
-        canvasCtx.lineTo(offsetX, offsetY);
-        canvasCtx.stroke();
-        lastX =offsetX;
-        lastY = offsetY;
-    }
-})
-//run when user starts moving inside of canvas
-canvas.addEventListener('mouseup' , (event) => {
+//run when user stops moving inside of canvas
+canvas.addEventListener('mouseup', (event) => {
     isDraw = false;
 })
+//is run when the user moves mouse inside of canvas 
+canvas.addEventListener('mousemove', (event) => {
+    draw(event);  
+})
+
 //run when users mouse leaves canvas
-canvas.addEventListener('mouseleave' , (event) => {
+canvas.addEventListener('mouseleave', (event) => {
     isDraw = false;
 })
 
+function init(event) {
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+}
+function draw(event) {
+    if (isDraw) {
+        canvasCtx.beginPath();
+        canvasCtx.moveTo(lastX, lastY);
+        canvasCtx.lineTo(event.offsetX, event.offsetY);
+        canvasCtx.stroke();
+        canvasCtx.closeParg();
+        lastX = event.offsetX;
+        lastY = event.offsetY;
+    }
+   
+}
 
 function showSideBar() {
     const sideBar = document.querySelector('.sidebar');
